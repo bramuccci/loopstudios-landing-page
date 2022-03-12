2b6ca1b2a1d6f4f0f021570f79823ec81f2f867f
Candela Bramucci <candelabramucci05@gmail.com>
2022-03-12 12:25:05 -0300

    nav styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import '@styles/main.scss'
+
+const mainNav = document.querySelector('.main-nav')
+const navToogle = document.querySelector('.main-nav__btn-toggle')
+
+navToogle.addEventListener('click', e => {
+    const visibility = mainNav.getAttribute('data-visible')
+
+    if (visibility === 'false') {
+        mainNav.setAttribute('data-visible', true)
+        navToogle.setAttribute('aria-expanded', true)
+    } else {
+        mainNav.setAttribute('data-visible', false)
+        navToogle.setAttribute('aria-expanded', false)
+    }
+})
