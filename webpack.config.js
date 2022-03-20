9198292c08bd994ca5ef8345cb8a27e21d68831f
Candela Bramucci <candelabramucci05@gmail.com>
2022-03-20 11:27:55 -0300

    fixing scroll in mobile nav and error in netlify.toml

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
-        // clean: true,
+        clean: true,
         assetModuleFilename: 'assets/[name].[hash].[ext]',
     },
     resolve: {
